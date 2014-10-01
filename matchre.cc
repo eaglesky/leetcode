acea5ed120460c226f6242b4b6609bc553b6ab49
eaglesky <allenchin1990@gmail.com>
2014-10-01 10:45:54 -0700

    expression matching updated

--- matchre.cc
+++ matchre.cc
@@ -2,84 +2,136 @@
 #include <cstring>
 using namespace std;
 
-// Exponential time and O(n) space
-/*bool isMatch0(const char *s, const char *p) {
+// Exponential time and O(ns + np) space
+ bool isMatch0(const char *s, const char *p) {
+        
+        if (!*p)
+            return !*s;
+            
+        if (*(p+1) == '*') {
+            while (1) {
+                if (isMatch(s, p+2))
+                    return true;
+                
+                if (((*s) && (*p == '.')) || (*s == *p))
+                    s++;
+                else
+                    break;
+            }
+            return false;
+        } else if (((*s) && (*p == '.')) || (*s == *p))
+            return isMatch(s+1, p+1);
+        else
+            return false;
+         
+    }
+
+
+ // DP solution.  O(N^2) time and O(N) space
+    bool isMatch1(const char *s, const char *p) {
+
+        int sLen = strlen(s);
+
+        int pLen = strlen(p);
 
         
 
-        if ((!*p) && (!*s))
+        bool* dp0 = new bool[sLen+1];
 
-            return true;
+        bool* dp1 = new bool[sLen+1];
 
-        else if (*p) {
+        fill_n(dp0, sLen+1, false);
 
-            if (*(p+1) != '*') {
+        fill_n(dp1, sLen+1, false);
 
-                if (((*p == '.') && (*s)) || (*s == *p))
+        dp1[0] = true;
 
-                    return isMatch(s+1, p+1);
+        for (int ip = 1; ip <= pLen; ++ip)
 
-                else
+        {
 
-                    return false;
+            bool* dpcur = new bool[sLen+1];
 
-            } else {
+            for (int is = 0; is <= sLen; ++is)
 
+            {
 
+                if ((p[ip-1] != '*')) {
 
-                while (((*s) && (*p == '.')) || (*p == *s))  {
+                    dpcur[is] = (is > 0) && ((s[is-1] == p[ip-1]) || (p[ip-1] == '.')) && dp1[is-1];
 
-                    if (isMatch(s, p+2))
+                } else {
 
-                        return true;
-
-                    s++;
+                    dpcur[is] = dp0[is] || ((is > 0) && ((s[is-1] == p[ip-2]) || (p[ip-2] == '.')) && (dpcur[is-1]));
 
                 }
 
-                return isMatch(s, p+2);
-
             }
 
-        } else
+            delete[] dp0;
 
-            return false;
+            dp0 = dp1;
+
+            dp1 = dpcur;
+
+        }
+
+        
+
+        bool ret = dp1[sLen];
+
+        
+
+        delete[] dp0;
+
+        delete[] dp1;
+
+        
+
+        return ret;
 
     }
-    */
- // DP solution.  O(N^2) time and O(N) space
-    bool isMatch(const char *s, const char *p) {
-        int sLen = strlen(s);
-        int pLen = strlen(p);
-        
-        bool* dp0 = new bool[sLen+1];
-        bool* dp1 = new bool[sLen+1];
-        fill_n(dp0, sLen+1, false);
-        fill_n(dp1, sLen+1, false);
-        dp1[0] = true;
-        for (int ip = 1; ip <= pLen; ++ip)
-        {
-            bool* dpcur = new bool[sLen+1];
-            for (int is = 0; is <= sLen; ++is)
-            {
-                if ((p[ip-1] != '*')) {
-                    dpcur[is] = (is > 0) && ((s[is-1] == p[ip-1]) || (p[ip-1] == '.')) && dp1[is-1];
-                } else {
-                    dpcur[is] = dp0[is] || ((is > 0) && ((s[is-1] == p[ip-2]) || (p[ip-2] == '.')) && (dpcur[is-1]));
-                }
-            }
-            delete[] dp0;
-            dp0 = dp1;
-            dp1 = dpcur;
-        }
-        
-        bool ret = dp1[sLen];
-        
-        delete[] dp0;
-        delete[] dp1;
-        
-        return ret;
+
+//DP solution improved. O(ns*np) time and O(np) space
+bool isMatch(const char *s, const char *p) {
+    int ns = strlen(s);
+    int np = strlen(p);
+    
+    bool* prev = new bool[np+1];
+    bool* next = new bool[np+1];
+    fill_n(prev, np+1, false);
+    fill_n(next, np+1, false);
+  
+    for (int i = ns; i >= 0; --i)
+    {
+        next[np] = (i == ns) ? true : false;
+
+        for (int j = np-1; j >= 0; --j)
+        {
+            
+            if (p[j] != '*') {
+                if ((j < np-1) && (p[j+1] == '*')) {
+                    next[j] = next[j+2];
+                    if ((i < ns) && ((p[j] == '.') || (s[i] == p[j])))
+                        next[j]  = next[j] || prev[j];
+                    
+                } else if ((i < ns) && ((p[j] == '.') || (s[i] == p[j])))
+                    next[j] = prev[j+1];
+                else
+                    next[j] = false;
+                
+            }
+            
+        }
+        swap(prev, next);
     }
+    
+    bool result = prev[0];
+    delete[] prev;
+    delete[] next;
+    return result;        
+}
+
 
 int main(int argc, char** argv)
 {
--- wildcard.cc
+++ wildcard.cc
@@ -2,66 +2,123 @@
 #include <iostream>
 using namespace std;
 
-//DP, TLE. O(Ns * Np) time, O(Ns) space
-    bool isMatch0(const char *s, const char *p) {
-        int sLen = strlen(s);
-        int pLen = strlen(p);
-        
-       const char* tmp = p;
-        int cnt = 0;
-        while (*tmp) if (*(tmp++) != '*') cnt++;  
-        if (cnt > sLen) return false;  
-            
-        bool* dp = new bool[sLen+1];
-        
-        fill_n(dp, sLen+1, false);
-        dp[0] = true;
-        
-        for (int j = 1; j <= pLen; ++j)
-        {
-            if (p[j-1] != '*') {
-                for (int i = sLen; i >= 0; --i)
-                {
-                    dp[i] = (i > 0) && ((s[i-1] == p[j-1])||(p[j-1] == '?')) && dp[i-1];
-                }
-            } else {
-                int i;
-                for (i = 0; (i <= sLen) && (!dp[i]); ++i) {}
-                for (; i <= sLen; ++i)
-                    dp[i] = true;
-            }
-        }
-        
-        bool ret = dp[sLen];
-        delete[] dp;
-        return ret;
+//Recursive solution . TLE
+/*bool isMatch(const char *s, const char *p) {
+        
+        if (!*p)
+            return !*s;
+            
+        if (((*s) && (*p == '?')) || (*s == *p))
+            return isMatch(s+1, p+1);
+            
+        if (*p == '*') {
+            do {
+                if (isMatch(s, p+1))
+                    return true;
+            } while(*s++);
+        } else
+            return false;
+        
+}*/
+
+//DP, TLE. O(Ns * Np) time, O(Ns) space
+    bool isMatch1(const char *s, const char *p) {
+        int sLen = strlen(s);
+        int pLen = strlen(p);
+        
+       const char* tmp = p;
+        int cnt = 0;
+        while (*tmp) if (*(tmp++) != '*') cnt++;  
+        if (cnt > sLen) return false;  
+            
+        bool* dp = new bool[sLen+1];
+        
+        fill_n(dp, sLen+1, false);
+        dp[0] = true;
+        
+        for (int j = 1; j <= pLen; ++j)
+        {
+            if (p[j-1] != '*') {
+                for (int i = sLen; i >= 0; --i)
+                {
+                    dp[i] = (i > 0) && ((s[i-1] == p[j-1])||(p[j-1] == '?')) && dp[i-1];
+                }
+            } else {
+                int i;
+                for (i = 0; (i <= sLen) && (!dp[i]); ++i) {}
+                for (; i <= sLen; ++i)
+                    dp[i] = true;
+            }
+        }
+        
+        bool ret = dp[sLen];
+        delete[] dp;
+        return ret;
+    }
+
+//DP, TLE. O(Ns * Np) time, O(Np) space
+ bool isMatch(const char *s, const char *p) {
+        int ns = strlen(s);
+        int np = strlen(p);
+        
+        bool* prev = new bool[np+1];
+        bool* cur = new bool[np+1];
+        
+        for (int i = ns; i >= 0; --i)
+        {
+            cur[np] = (i == ns) ? true : false;
+            for (int j = np-1; j >= 0; --j)
+            {
+                if ((i < ns) && ((p[j] == '?') || (s[i] == p[j])))
+                    cur[j] = prev[j+1];
+                else if (p[j] == '*') {
+                    cur[j] = cur[j+1];
+                    if (i < ns)
+                        cur[j] = cur[j] || prev[j];
+                }
+                else
+                    cur[j] = false;
+            }
+            
+            swap(prev, cur);
+        }
+        
+        bool result = prev[0];
+        delete[] prev;
+        delete[] cur;
+        return result;
     }
 
 // O(Ns^2) time and O(1) space
- bool isMatch(const char *s, const char *p) {
-        
-        const char* preStar = NULL;
-        const char* preS = s;
-        while(*s) {
-            if ((*s == *p) || (*p == '?')) {
-                s++;
-                p++;
-            } else if (*p == '*') {
-                preStar = p++;
-                preS = s;
-            } else if (preStar) {
-                p = preStar + 1;
-                s = ++preS;
-            } else 
-                return false;
-        }
-        
-        while (*p == '*') p++;
-        return !*p;
+// This is essentially a backtracking algorithm.
+// The key is that when you backtrack, you go to the last star positions, not
+// the first one. Since the strings before the last star are already matched,
+// and also the matched substring of s is the shortest one, so you should start
+// from the next position after that.
+ bool isMatch(const char *s, const char *p) {
+        
+        const char* preStar = NULL;
+        const char* preS = s;
+        while(*s) {
+            if ((*s == *p) || (*p == '?')) {
+                s++;
+                p++;
+            } else if (*p == '*') {
+                preStar = p++;
+                preS = s;
+            } else if (preStar) {
+                p = preStar + 1;
+                s = ++preS;
+            } else 
+                return false;
+        }
+        
+        while (*p == '*') p++;
+        return !*p;
     }
 
 int main(int argc, char** argv)
 {
-    cout << isMatch("aaaa", "aa*b") << endl; 
+    cout << isMatch("aaaa", "aa*") << endl; 
     return 0;
 }
