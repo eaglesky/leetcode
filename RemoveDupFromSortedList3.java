d57d1b8f30110714147b0921c106b549319342d8
eaglesky <allenchin1990@gmail.com>
2016-10-14 00:04:03 -0700

    Complete RemoveDupFromSortedList3.java

--- RemoveDupFromSortedList3.java
+++ RemoveDupFromSortedList3.java
@@ -1,12 +1,12 @@
 public class RemoveDupFromSortedList3 {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode(int x) { val = x; }
     }
 
-    public ListNode deleteDuplicatesLeaveK(ListNode head, int k) {
+    public static ListNode deleteDuplicatesLeaveK(ListNode head, int k) {
         if (head == null || head.next == null) {
             return head;
         }
@@ -23,18 +23,39 @@ public class RemoveDupFromSortedList3 {
             int i = 0;
             for (; i < k && (pre.next != cur); pre = pre.next, ++i);
             if (i < k || ((k == 0) && (pre.next == cur))) {
-                pre = pre.next;
-                cur = cur.next;
+                pre = pre.next;   
             } else {
                 pre.next = cur.next;
-                cur = cur.next;
             }
+            cur = cur.next;
         }
         return dummy.next;
     }
 
+    private static void printLinkedList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            System.out.print(cur.val + ", ");
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
-        int k = 2;
-        
+        int k = 3;
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        for (String arg: args) {
+            ListNode curNode = new ListNode(Integer.parseInt(arg));
+            tail.next = curNode;
+            tail = curNode;
+        }
+        System.out.println("Input linked list: ");
+        printLinkedList(dummy.next);
+
+        System.out.println("After removing duplicates:");
+        printLinkedList(deleteDuplicatesLeaveK(dummy.next, k));
+
     }
 }
