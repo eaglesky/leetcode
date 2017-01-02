a83f005893a3889c746c5b9ac28e6c646955bd0e
Yalun Qin <yqin@rocketfuel.com>
2017-01-02 10:32:22 +0800

    Add optimal solution to RegularExpressionMatching.java

--- RegularExpressionMatching.java
+++ RegularExpressionMatching.java
@@ -1,7 +1,9 @@
+import java.util.*;
+
 public class RegularExpressionMatching {
     
-    //DP solution, O(ls*lp) time and O(ls) space
-    public boolean isMatch(String s, String p) {
+    //DP solution, O(ls*lp*ls) time and O(ls) space
+    public boolean isMatch0(String s, String p) {
         boolean[] pre2 = new boolean[s.length() + 1];
         boolean[] pre = new boolean[s.length() + 1];
         pre[0] = true;
@@ -37,5 +39,40 @@ public class RegularExpressionMatching {
             pre = temp;
         }
         return pre[s.length()];
+    }
+
+    //DP solution, O(ls*lp) time and O(ls) space
+    public boolean isMatch(String s, String p) {
+        boolean[] pre2 = new boolean[s.length() + 1];
+        boolean[] pre = new boolean[s.length() + 1];
+        boolean[] cur = new boolean[s.length() + 1];
+        pre[0] = true;
+        for (int lp = 1; lp <= p.length(); lp++) {
+            for (int ls = 0; ls <= s.length(); ls++) {
+                if (p.charAt(lp-1) == '.') {
+                    cur[ls] = ls > 0 && pre[ls-1];
+                } else if (p.charAt(lp-1) == '*') {
+                    cur[ls] = lp > 1 
+                    && (pre2[ls] 
+                        || (ls > 0 && (p.charAt(lp-2) == '.' || p.charAt(lp-2) == s.charAt(ls-1)) && cur[ls-1]));
+                } else {
+                    cur[ls] = ls > 0 && (p.charAt(lp-1) == s.charAt(ls-1)) && pre[ls-1];
+                }
+            }
+            boolean[] temp = pre2;
+            pre2 = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre[s.length()];
+    }
+
+    public static void main(String[] args) {
+    	String s = "aa";
+    	String p = "aa";
+    	System.out.println("s = " + s);
+    	System.out.println("p = " + p);
+    	RegularExpressionMatching rem = new RegularExpressionMatching();
+    	System.out.println(rem.isMatch(s, p));
     }
 }
