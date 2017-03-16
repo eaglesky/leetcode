dc0581762fa7d2fee085019785d22a9cded45626
Yalun Qin <yqin@rocketfuel.com>
2017-03-16 00:51:05 -0700

    Add SubMatrixSum.java

---
+++ Java/SubMatrixSum.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class SubMatrixSum {
+
+	//O(n^3) time and O(n^2) space
+	//Can also be solved in O(n) space, but trickier:
+	//http://www.cnblogs.com/grandyang/p/5814131.html
+    public int[][] submatrixSum(int[][] matrix) {
+        int[][] result = new int[2][2];
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        int target = 0;
+        int[][] subsums = new int[matrix.length+1][matrix[0].length+1];
+        for (int i = 1; i < subsums.length; ++i) {
+            for (int j = 1; j < subsums[i].length; ++j) {
+                subsums[i][j] = subsums[i][j-1] + subsums[i-1][j] - subsums[i-1][j-1]
+                                + matrix[i-1][j-1];
+            }
+        }
+        for (int r1 = 0; r1 < subsums.length; ++r1) {
+            for (int r2 = r1 + 1; r2 < subsums.length; ++r2) {
+                Map<Integer, Integer> diffToCol = new HashMap<>();
+                diffToCol.put(0, 0);
+                for (int c = 1; c < subsums[r2].length; ++c) {
+                    int subsum = subsums[r2][c] - subsums[r1][c];
+                    int diff = subsum - target;
+                    Integer prevCol = diffToCol.get(diff);
+                    if (prevCol != null) {
+                        result[0][0] = r1;
+                        result[0][1] = prevCol;
+                        result[1][0] = r2 - 1;
+                        result[1][1] = c - 1;
+                        return result;
+                    }
+                    diffToCol.put(subsum, c);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+    	SubMatrixSum solution = new SubMatrixSum();
+    	int[][] matrix = new int[][] {
+    		{1,1,1,1,1,1,1,1,1,1,1,-10,1,1,1,1,1,1,1,1,1,1,1}
+    	};
+    	int[][] result = solution.submatrixSum(matrix);
+    	System.out.println(Arrays.toString(result[0]));
+    	System.out.println(Arrays.toString(result[1]));
+    }
+}
