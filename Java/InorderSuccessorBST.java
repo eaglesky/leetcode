eeba72a68ed29e125319fbabe0a4ec6be4ea7595
Yalun Qin <yqin@rocketfuel.com>
2017-07-17 00:00:22 -0700

    Update InorderSuccessorBST.java

--- Java/InorderSuccessorBST.java
+++ Java/InorderSuccessorBST.java
@@ -1,3 +1,9 @@
+/*
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+Note: If the given node has no in-order successor in the tree, return null.
+*/
+
 public class InorderSuccessorBST {
 
 	//O(h) time and O(1) space
