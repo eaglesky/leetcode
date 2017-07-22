9355bb40685c61bc1c3578bc1986bb99d2227c7b
Yalun Qin <yqin@rocketfuel.com>
2017-07-22 15:38:01 -0700

    Update SearchForRange.java

--- Java/SearchForRange.java
+++ Java/SearchForRange.java
@@ -31,4 +31,37 @@ public class SearchForRange {
         int upperBound = searchBound(nums, target ,false);
         return new int[]{lowerBound, upperBound};
     }
+
+    //Best implementation:
+    private int getLowerOrUpperBound(int[] nums, int target, boolean isLowerBound) {
+        int low = 0;
+        int high = nums.length - 1;
+        int foundId = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target > nums[mid]) {
+                low = mid + 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                foundId = mid;
+                if (isLowerBound) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return foundId;
+    }
+    
+    public int[] searchRange(int[] nums, int target) {
+        int lowerId = getLowerOrUpperBound(nums, target, true);
+        if (lowerId < 0) {
+            return new int[]{-1, -1};
+        } else {
+            int upperId = getLowerOrUpperBound(nums, target, false);
+            return new int[]{lowerId, upperId};
+        }
+    }
 }
