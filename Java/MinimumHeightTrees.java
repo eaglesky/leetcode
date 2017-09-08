4065db37e4a90851015458d96b169edb6f8f211d
Yalun Qin <yqin@rocketfuel.com>
2017-09-08 00:37:41 -0700

    Update MHT

--- Java/MinimumHeightTrees.java
+++ Java/MinimumHeightTrees.java
@@ -1,6 +1,20 @@
 public class MinimumHeightTrees {
     
     //O(n) time and O(n+) space.
+    //In each tree, the height is equal to the distance from the root
+    //to the furthest leaf. The root of MHT must be located on the 
+    //longest path between too leaves(say LP), and must be the middle
+    //node(s) of LP. Why? For any node outside LP, it must be able to
+    //reach LP since the graph is a tree, and there must be a path between
+    //that node to a leaf that is longer than the dis between LP middle 
+    //node and that leaf. Even if there could be multiple LPs, the root
+    //of MHT has to be the middle node(s) of one of them, using simlar
+    //proof like before. So the result must contain at most 2 nodes.
+    //To find out them, we can iteratively remove leaves of the graph
+    //until there are no more than 2 nodes left. Then they are the middle
+    //nodes -- root of MHT. The reason why this algorithm works is that
+    //everytime you remove the leaves, the length of LP must be reduced
+    //by 2, and the length of LP left must be still the maximum length.
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         List<Integer> result = new ArrayList<>();
         List<Set<Integer>> adjs = new ArrayList<>(n);
