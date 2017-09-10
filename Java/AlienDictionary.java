efe0d9feab6912dfc352ca3b6ce13468bcb53cab
Yalun Qin <yqin@rocketfuel.com>
2017-09-09 19:44:17 -0700

    Update Alien Dictionary

--- Java/AlienDictionary.java
+++ Java/AlienDictionary.java
@@ -65,4 +65,57 @@ public class AlienDictionary {
         }
         return topoSort(adjGraph, charSet);
     }
+
+    //BFS solution. Slow
+    private String getTopoSort(Map<Character, Set<Character>> graph) {
+        Map<Character, Integer> inBounds = new LinkedHashMap<>();
+        for (Map.Entry<Character, Set<Character>> entry : graph.entrySet()) {
+            inBounds.putIfAbsent(entry.getKey(), 0);
+            for (Character c : entry.getValue()) {
+                int curNum = inBounds.getOrDefault(c, 0);
+                inBounds.put(c, curNum + 1);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : inBounds.entrySet()) {
+            if (entry.getValue() == 0) {
+                sb.append(entry.getKey());
+            }
+        }
+        for (int i = 0; i < sb.length(); ++i) {
+            char c = sb.charAt(i);
+            for (char adjC : graph.get(c)) {
+                inBounds.put(adjC, inBounds.get(adjC) - 1);
+                if (inBounds.get(adjC) == 0) {
+                    sb.append(adjC);
+                }
+            }
+        }
+        return (sb.length() < graph.size()) ? "" : sb.toString();
+    }
+    
+    public String alienOrder(String[] words) {
+        if (words == null) {
+            return "";
+        }
+        Map<Character, Set<Character>> graph = new LinkedHashMap<>();
+        String prevWord = "";
+        for (String curWord : words) {
+            int j = 0;
+            for (int i = 0; i < prevWord.length() && j < curWord.length(); ++i, ++j) {
+                char prevC = prevWord.charAt(i);
+                char curC = curWord.charAt(j);
+                graph.computeIfAbsent(curC, k -> new LinkedHashSet<>());
+                if (prevC != curC) {
+                    graph.get(prevC).add(curC);
+                    break;
+                }
+            }
+            for (; j < curWord.length(); ++j) {
+                graph.computeIfAbsent(curWord.charAt(j), k -> new LinkedHashSet<>());
+            }
+            prevWord = curWord;
+        }
+        return getTopoSort(graph);
+    }
 }
