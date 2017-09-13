0290c697f5d55345ec0259ce198d414894aff6dc
Yalun Qin <yqin@rocketfuel.com>
2017-09-13 12:32:09 -0700

    Update HIndex2

--- Java/FirstBadVersion.java
+++ Java/FirstBadVersion.java
@@ -6,7 +6,9 @@ public class FirstBadVersion extends VersionControl {
     //Binary search. O(logn) time and O(1) space
     public int firstBadVersion(int n) {
         int low = 1;
-        int high = n;
+        
+        //Cannot be n+1 since it would overflow if n is max_integer.
+        int high = n; 
         while (low < high) {
             int mid = low + (high - low) / 2;
             if (isBadVersion(mid)) {
@@ -16,5 +18,22 @@ public class FirstBadVersion extends VersionControl {
             }
         }
         return isBadVersion(low) ? low : 0;
+    }
+
+    //Another algorithm. Minimum calls to isBadVersion
+    public int firstBadVersion(int n) {
+        int id = 0;
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) {
+                id = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return id;
     }
 }
--- Java/HIndex2.java
+++ Java/HIndex2.java
@@ -21,4 +21,20 @@ public class Solution {
         }
         return low;
     }
+
+    //Another implementation
+    public int hIndex(int[] citations) {
+        int low = 0;
+        int high = citations.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int h = citations.length - mid;
+            if (citations[mid] >= h) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return citations.length - low;
+    }
 }
