ecb032981e95c6586deb09376b8baba334e83b5c
Yalun Qin <yqin@rocketfuel.com>
2017-09-21 11:38:19 -0700

    Update SparseMatrixMultiplication.java

--- Java/SparseMatrixMultiplication.java
+++ Java/SparseMatrixMultiplication.java
@@ -1,3 +1,27 @@
+/*
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+*/
+                  
 public class SparseMatrixMultiplication {
     
     //Naive soltuion. TLE. If A is m x n, B is n x t, then it is O(mnt) time.
