0d1e071b70d19083fd225defb882b785f2e8a699
Yalun Qin <yqin@rocketfuel.com>
2017-10-25 00:44:49 -0700

    Update its

--- Java/IncreasingTripletSubsequence.java
+++ Java/IncreasingTripletSubsequence.java
@@ -1,5 +1,25 @@
 public class IncreasingTripletSubsequence {
     
+    
+    public boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) {
+            return false;
+        }
+        int prevMin = nums[0];
+        Integer prevBiggerMin = null;
+        for (int i = 1; i < nums.length; ++i) {
+            if (prevBiggerMin != null && nums[i] > prevBiggerMin) {
+                return true;
+            }
+            if (nums[i] > prevMin) {
+                prevBiggerMin = nums[i];
+            } else {
+                prevMin = nums[i];
+            }
+        }
+        return false;
+    }
+
     //O(n) time and O(1) space
     //Can be easily changed to return the triplet.
     public boolean increasingTriplet(int[] nums) {
